26fa0375ddeddfdefdc88efd039455846fb30373
NerdcoreSteve <dontworshipsteve@gmail.com>
2014-06-08 16:51:41 -0600

    added redirect for when user is not authorized to view something

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  private
+
+  def user_not_authorized
+    flash[:error] = "You are not authorized to perform this action."
+    #TODO can the user be redirected to whatever the previous page was?
+    redirect_to(request.referrer || root_path)
+  end
 end
