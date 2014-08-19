92990fa49e07eae20a853a18b637a68e55407047
NerdcoreSteve <dontworshipsteve@gmail.com>
2014-08-19 12:34:47 -0600

    changed bucket item description to notes

--- app/models/bucket_item.rb
+++ app/models/bucket_item.rb
@@ -1,4 +1,4 @@
 class BucketItem < ActiveRecord::Base
   belongs_to :user
-  validates :name, :bucket_item_type, :bucket, :description, :status, presence: true
+  validates :name, :bucket_item_type, :bucket, :notes, :status, presence: true
 end
--- app/views/bucket_items/show.json.jbuilder
+++ app/views/bucket_items/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @bucket_item, :id, :name, :bucket_item_type, :bucket, :description, :status, :created_at, :updated_at
+json.extract! @bucket_item, :id, :name, :bucket_item_type, :bucket, :notes, :status, :created_at, :updated_at
